a8604aa5cf7254b2c44210e07e2386c894e41947
daichi-miura <dai0531m@gmail.com>
2021-06-29 12:45:24 +0900

    modify Q&A

--- src/pages/vending_booth/frequently_asked_questions/index.js
+++ src/pages/vending_booth/frequently_asked_questions/index.js
@@ -29,20 +29,36 @@ export default class Index extends React.Component {
                   <div className="faq">
                     <dl>
                       <div className="faq-content">
-                        <dt className="question">予約をキャンセルしたいのですが、どうすればいいですか？</dt>
-                        <dd className="answer">キャンセル期限が過ぎた商品のキャンセルはお受けできません。キャンセルが可能な商品については、受け取り画面より商品を選択し、注文をキャンセルしてください。</dd>
-                      </div>
-                      <div  className="faq-content">
                         <dt className="question">カードの登録ができません</dt>
-                        <dd className="answer">カード情報についてのお問合せは当社では受け付けておりません。お使いのカード会社にお問い合わせいただきますようお願いします。</dd>
-                      </div>
-                      <div  className="faq-content">
-                        <dt className="question">予約した商品が置かれていません</dt>
-                        <dd className="answer">ご注文いただいた商品が届いておらず申し訳ございませんでした。お気づきになった場合はお問合せフォームよりご連絡ください。速やかに個別に対応させていただきます。</dd>
+                        <dd className="answer">カード情報についてのお問合せは当社では受け付けておりません。お使いのカード会社にお問合せいただきますようお願いします。</dd>
                       </div>
                       <div  className="faq-content">
                         <dt className="question">商品に不備や異常を感じる場合</dt>
-                        <dd className="answer">商品に異常を感じた場合は速やかにお問合せフォームよりご連絡ください。</dd>
+                        <dd className="answer">商品に異常を感じた場合は、速やかに「ホーム画面」→「設定」→「お問い合わせ」よりご連絡ください。</dd>
+                      </div>
+                      <div  className="faq-content">
+                        <dt className="question">アプリ上に表示されている商品がありません</dt>
+                        <dd className="answer">品切れ中だと思われますので、いつ納品されるかは「ホーム画面」→「設定」→「お問い合わせ」よりご連絡ください</dd>
+                      </div>
+                      <div  className="faq-content">
+                        <dt className="question">初めて使うため、使い方に不安があります</dt>
+                        <dd className="answer">今までの買い物と異なる体験なので、皆様不安や心配を感じられますが、いたってシンプルです。ダウンロードしたアプリを使ってQR認証、後は欲しい商品を持って帰るだけです！使い方ガイダンスを再度ご覧になりたい場合、「ホーム画面」→「設定」→「ご利用方法」を参照ください。</dd>
+                      </div>
+                      <div  className="faq-content">
+                        <dt className="question">どのように無人化を実現しているのですか？</dt>
+                        <dd className="answer">当社の画像AIの技術を使って、「誰が何を」棚から取ったか認識することで無人化を実現しております。通常のお買い物と同様、商品を手にしてブースから離れると、「退店」したとみなし、後から取った商品のご請求をさせて頂きます。</dd>
+                      </div>
+                      <div  className="faq-content">
+                        <dt className="question">持ち帰った商品の通知がなかなか来ないのですが、大丈夫でしょうか？</dt>
+                        <dd className="answer">お持ち帰りになられた商品の通知は受け取り後、最大1日以内に通知されるので、それまではお待ちください。もし通知が1日経っても来ない場合、「ホーム画面」→「設定」→「お問い合わせ」からご連絡頂けると幸いです。</dd>
+                      </div>
+                      <div  className="faq-content">
+                        <dt className="question">持ち帰った商品の通知と、実際に持ち帰った商品が違いました</dt>
+                        <dd className="answer">大変申し訳ありません。微々たる確率ですが、認識にズレが発生する場合があります。そのため、通知画面もしくは利用履歴画面から、受取商品の修正をすることが可能となっております。お手数ですが、そちらから商品の修正をよろしくお願いします。なお、修正ができるのは通知の受取後、48時間以内となっておりますので、通知をご覧頂いたら速やかにご対応をよろしくお願いします。</dd>
+                      </div>
+                      <div  className="faq-content">
+                        <dt className="question">アプリおよびブースのサービスに不具合がありました</dt>
+                        <dd className="answer">大変申し訳ありません。不具合に関するお問い合わせは、「ホーム画面」→「設定」→「お問い合わせ」より当社までご連絡ください。</dd>
                       </div>
                     </dl>
                   </div>
